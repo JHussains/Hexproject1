198c1db17b4e69cb00039df5498d55591f460533
Javed Hussain <66272705+JHussains@users.noreply.github.com>
2020-06-09 04:55:02 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,21 +7,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Created by Javed Hussain
-        </p>
-        <a
-          className="App-link"
-          href="https://getinfoline.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Please visit my web page 
-        </a>
+          <h2>Welcome to iOS Application </h2>
       </header>
+            <a className="App-link"
+          href="https://getinfoline.com"
+          target="_blank"rel="noopener noreferrer">
+          Click Here 
+        </a>
+    <h1>Piaic Offering Best Following Courses</h1>
+          <center>
+          <fieldset><legend>Piaic Offering</legend></fieldset>
+          <h4>Created by:Javed Hussain</h4>
+          </center>
     </div>
   );
 }
 
 export default App;
-
