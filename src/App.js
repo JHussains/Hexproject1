5e305dca746a9ded3dcba39d06fa54d376391c3e
Javed Hussain <66272705+JHussains@users.noreply.github.com>
2020-06-09 05:03:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,15 +9,21 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to iOS Application </h2>
       </header>
+          <br />
             <a className="App-link"
           href="https://getinfoline.com"
           target="_blank"rel="noopener noreferrer">
-          Click Here 
+          Visit Our Website 
         </a>
     <h1>Piaic Offering Best Following Courses</h1>
           <center>
-          <fieldset><legend>Piaic Offering</legend></fieldset>
-          <h4>Created by:Javed Hussain</h4>
+          <fieldset><legend>Piaic Offering</legend>
+            <h3>Artifical Intelligence</h3>
+            <h3>Cloud Native Computing</h3>
+            <h3>Blockchain</h3>
+            <h3>Internet of Things</h3>
+          </fieldset>
+          <h4><font color"yellowgreen">Created by: Javed Hussain</font></h4>
           </center>
     </div>
   );
